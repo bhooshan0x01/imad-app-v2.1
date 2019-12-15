6c8536f9bd7a37706881f4c3102dce6521d2c36e
Bhushan Ramteke <bhushan7667@gmail.com>
2019-12-15 15:44:37 +0530

    compact code without excess html file

--- server.js
+++ server.js
@@ -5,21 +5,101 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	//articel object for storing the content which is changing in each artile page
+	'article-one': {
+		title: 'Article One | Bhushan',
+		heading: 'Article One',
+		date: 'Dec 15, 2019',
+		content: `
+			<p>
+				I am enthusiastic about doing and learning the programming but
+				I lost my motivation right in the middle of the learning 
+				when I can't able to solve or do the right amout of task.
+			</p>
+		`
+		},
+	'article-two': {
+		title: 'Article Two | Bhushan',
+		heading: 'Article Two',
+		date: 'Dec 16, 2019',
+		content: `
+			<p>
+				But I am so dumb I always get scared of little things.
+			</p>
+		`
+		},
+	'article-three': {
+		title: 'Article Three | Bhushan',
+		heading: 'Article Three',
+		date: 'Dec 17, 2019',
+		content: `
+			<p>
+				I think I should have birth as a tortoise.
+				I am so lame I don't want to use my brain.
+				<hr/>
+				Anyways for keeping my faith I use brain that's why I am able to copy and paste this page from external source.	
+			</p>
+		`
+		}
+
+};
+
+function createTemplate (data) {
+
+	var title = data.title;
+	var date = data.date;
+	var heading = data.heading;
+	var content = data.content;
+
+	var htmlTemplate = `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>
+			${title} 
+		</title>
+		<!--- For mobile view use below meta---->
+    	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    	<link href="/ui/style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="container">
+		
+		<div>
+			<a href="/">Home</a>
+		</div>
+		<hr/>
+		<h3>
+			${heading} <!--Heading-->
+		</h3>
+		<div>
+			${date} <!--Date-->
+		</div>
+		<div>
+			${content}<!--Content-->
+		</div>
+
+    	</div>
+	</body>
+	</html>
+
+	`;
+
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+	//articleName == articel-one
+	//articles[articleName] == {} content object for article one
+	var articleName = req.params.articleName; //express framework utility
+  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send('Article two is requested.');
-});
-
-app.get('/article-three', function (req, res) {
-  res.send('Article three is requested.');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
